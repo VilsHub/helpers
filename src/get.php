f890c0d9d98e7c1795dd981e4112391317cd8017
Starlyvil <stalyvil@gmail.com>
2020-01-22 03:11:03 +0100

    ready for v1.0.0

--- src/get.php
+++ src/get.php
@@ -23,5 +23,20 @@
       };
       return "<b style='color:#8a1313'>{$className}::{$methodName}()</b> ";
     }
+    public static function nonStaticMethod ($className, $methodName){
+      $ErrorStyle = " style='box-sizing: border-box; border:solid red 1px; color:red; width:100%; height:auto ; background-color:#f0cfcf; padding:10px;'";
+      try{
+        if(!is_string($className)){
+          $iError = "<b style='color:#8a1313'>".__CLASS__."::".__FUNCTION__."()</b> static method argument 1 must be string representing class name";
+          throw new Exception("<div{$ErrorStyle}>".$iError."</div>");
+        }else if (!is_string($methodName)) {
+          $iError = "<b style='color:#8a1313'>".__CLASS__."::".__FUNCTION__."()</b> static method argument 2 must be string representing method name";
+          throw new Exception("<div{$ErrorStyle}>".$iError."</div>");
+        }
+      }catch(Exception $e){
+        die(message::write("error", $e->getMessage()));
+      };
+      return "<b style='color:#8a1313'>{$className}->{$methodName}()</b> ";
+    }
   }
 ?>
