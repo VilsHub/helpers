5a91deccf2f2b17b344f8ba3ade8b89d1c9b35b2
Starlyvil <starlyvil@gmail.com>
2021-10-06 23:35:46 +0100

    Optimised error stylings

--- src/message.php
+++ src/message.php
@@ -10,7 +10,7 @@
     public static function write ($type, $msg){
       $typeLower = strtolower($type);
       $msgLower = strtolower($msg);
-      $ErrorStyle = " style='box-sizing: border-box; border:solid red 1px; color:red; width:98%; height:auto; background-color:#f0cfcf; padding:10px; margin:0 auto'";
+      $ErrorStyle = " style='box-sizing: border-box; border:solid red 1px; color:red; width:98%; height:auto; background-color:#f0cfcf; padding:10px; margin:10px auto'";
       try{
         if ($typeLower != "error" && $typeLower != "warning"){
           $iError = "<b style='color:#8a1313'>".__CLASS__."::".__FUNCTION__."()</b> static method argument 1 must either be: 'error' or 'warning'";
@@ -24,6 +24,7 @@
       }
       $fullMessage = "";
       if($typeLower == "error"){
+        // header("HTTP/1.0 500 Server error");
         $fullMessage = "<div{$ErrorStyle}>".$msg."</div>";
       }
       return $fullMessage;
